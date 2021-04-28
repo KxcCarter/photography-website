2fc18aa6106b2015932d93137b21d1d3d5bfc099
Kenneth Carter <kxccarter@gmail.com>
2021-04-28 10:05:58 -0500

    building carousel.

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Wrapper } from './HeroSlider.styles';
-import photo from '../images/image5.jpg';
+import photo1 from '../images/image1.jpg';
+import photo2 from '../images/image2.jpg';
+import photo3 from '../images/image3.jpg';
+import photo4 from '../images/image4.jpg';
+import photo5 from '../images/image5.jpg';
+
+const IMAGES = [photo1, photo2, photo3, photo4, photo5];
+
+const renderedImages = IMAGES.map((image, index) => (
+  <img
+    className="image"
+    src={image}
+    alt="I took these photos and you should be impressed."
+  />
+));
 
 const HeroSlider = (props) => {
   return (
     <Wrapper>
       <div className="view-container">
-        <img className="image" src={photo} alt="Victor Portrait" />
+        <div className="slide"></div>
+        <img className="image" src={photo1} alt="Victor Portrait" />
         <div class="center">
           <h1>Style</h1>
         </div>
+        {/* rename this class \/ */}
         <div class="topleft">
           <h1>H. Raymond Carter Photography</h1>
         </div>
