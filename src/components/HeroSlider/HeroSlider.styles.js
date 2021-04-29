b3052b14789e4ec5b197563d42b4361735d5b479
Kenneth Carter <kxccarter@gmail.com>
2021-04-28 21:05:48 -0500

    Had to revert to previous styles for HeroSlider because of unusual behavior.

--- src/components/HeroSlider/HeroSlider.styles.js
+++ src/components/HeroSlider/HeroSlider.styles.js
@@ -5,33 +5,27 @@ export const Wrapper = styled.div`
   text-align: center;
   overflow: hidden;
   padding-bottom: -20px;
-
   .view-container {
     position: relative;
     width: 100%;
     height: 700px;
   }
-
   .slide {
     opacity: 0;
     transform: scale(1.25);
     transition-duration: 2s ease-in-out;
   }
-
   .active {
     opacity: 1;
     transform: scale(1);
     transition-duration: 2s;
     transition-timing-function: ease-in-out;
   }
-
   .view-container img {
     max-width: 100%;
     transform: translateY(-25%);
   }
-
   /* TEXT */
-
   .info-center h1 {
     position: absolute;
     font-size: 5rem;
@@ -42,9 +36,7 @@ export const Wrapper = styled.div`
     z-index: 30;
     transform: translate(-50%, -50%);
   }
-
   /* NAVIGATION */
-
   .arrow {
     position: absolute;
     z-index: 101;
@@ -54,71 +46,56 @@ export const Wrapper = styled.div`
     cursor: pointer;
     user-select: none;
   }
-
   .arrow:hover {
     box-shadow: 1px 2px 3px rgba(10, 10, 10, 0.4);
     border-radius: 50%;
     color: rgba(210, 210, 210, 0.9);
   }
-
   .arrow-left {
     left: 16px;
   }
-
   .arrow-right {
     right: 16px;
   }
-
   /* MEDIA QUERIES */
   @media (max-width: 1024px) {
     aspect-ratio: 4/4;
-
     .view-container img {
       max-height: 100%;
       transform: translateY(-10%);
     }
-
     .info-center {
       /* transform: translateY(-50%); */
       /* font-size: 6rem; */
-
       top: 300px;
     }
-
     h1 {
       font-size: 3rem;
     }
   }
-
   @media (max-width: 800px) {
     .arrow {
       font-size: 2rem;
     }
   }
-
   @media (max-width: 600px) {
-    aspect-ratio: 4/5;
-
+    aspect-ratio: 3/4;
     .view-container img {
       max-height: 100%;
       transform: translateY(0);
     }
-
     /* Not sure if I need a rule for .image and img */
     .image {
       width: 100%;
       height: 100%;
     }
-
     h1 {
       font-size: 1rem;
     }
-
     .info-center h1 {
       font-size: 2rem;
       top: 50%;
     }
-
     .arrow {
       font-size: 1.6rem;
       top: 50%;
